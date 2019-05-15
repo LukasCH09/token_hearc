68645e99b0f768b937d99166c31aa4315deb7090
lukas.bitter <lukas.bitter@gmail.com>
2019-05-15 21:49:02 +0200

    3. Delegate Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -19,7 +19,14 @@ contract DappToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
     // ------------------------------------------------------------------------
     // Constructor
     // -------------------------------------------------------------------- ----
@@ -37,6 +44,28 @@ contract DappToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(_to != address(0), "ERC20: transfer to the zero address");
+
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }

4b4a1f0b01cb5a7520092736cd586fb85680dc79
lukas.bitter <lukas.bitter@gmail.com>
2019-05-19 12:58:54 +0200

    4. End sale

---
+++ contracts/DappTokenSale.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.0;
+
+import "./DappToken.sol";
+import "./DappToken.sol";
+import "./SafeMath.sol";
+
+contract DappTokenSale {
+	address admin;
+	DappToken public tokenContract;
+	//SafeMath public tokenMath;
+	uint256 public tokenPrice;
+	uint256 public tokenSold;
+	
+	using SafeMath for uint256;
+	
+	event Sell(
+		address _buyer,
+		uint256 _amount
+		);
+
+	constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
+		admin = msg.sender;
+		tokenContract = _tokenContract;
+		tokenPrice = _tokenPrice;
+	}
+
+	function multiply(uint x, uint y) internal pure returns (uint z) {
+		require(y == 0 || (z = x * y) / y == x);
+	}
+
+	function buyTokens(uint256 _numberOfTokens) public payable {
+		//require(msg.value == multiply(_numberOfTokens, tokenPrice));
+		require(msg.value == _numberOfTokens.mul(tokenPrice));
+		require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+		require(tokenContract.transfer(msg.sender, _numberOfTokens));
+		
+		tokenSold += _numberOfTokens;
+		emit Sell(msg.sender, _numberOfTokens);
+
+	}
+
+	function endSale() public payable {
+		require(msg.sender == admin);
+		require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+		//address payable admin_pay = admin;
+		//address payable testPayable = address(uint160(msg.sender));
+		selfdestruct(msg.sender);
+
+	}
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
 const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
 const SafeMath = artifacts.require("SafeMath");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(function() {
+  	// Token price is 0.001 Ether
+  	var tokenPrice = 1000000000000000;
+  	return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);	
+  });  
+
 };
